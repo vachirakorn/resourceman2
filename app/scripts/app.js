705dea5d0f5018ea146359decc09df2a45c41863
Vachirakorn Rungsikawanich <vachirakorn@hotmail.com>
2016-06-15 23:31:53 +0700

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularGanttDemoApp
+ * @description
+ * # angularGanttDemoApp
+ *
+ * Main module of the application.
+ */
+angular.module('angularGanttDemoApp', [
+    'gantt', // angular-gantt.
+    'gantt.sortable',
+    'gantt.movable',
+    'gantt.drawtask',
+    'gantt.tooltips',
+    'gantt.bounds',
+    'gantt.progress',
+    'gantt.table',
+    'gantt.tree',
+    'gantt.groups',
+    'gantt.dependencies',
+    'gantt.overlap',
+    'gantt.resizeSensor',
+    'ngAnimate',
+    'mgcrea.ngStrap',
+    'color.picker',
+    'ngSanitize'
+]).config(['$compileProvider','$asideProvider', function($compileProvider,$asideProvider) {
+    $compileProvider.debugInfoEnabled(false); // Remove debug info (angularJS >= 1.3)
+    angular.extend($asideProvider.defaults, {
+              templateUrl: 'template/aside.task.tpl.html',
+              animation: 'am-slide-bottom',
+              placement: 'bottom'
+            });
+}]);
---
+++ app/scripts/services/sample.js
@@ -0,0 +1,242 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularGanttDemoApp.Sample
+ * @description
+ * # Sample
+ * Service in the angularGanttDemoApp.
+ */
+angular.module('angularGanttDemoApp')
+    .service('Sample', function Sample() {
+        return {
+            getResourceSampleData: function() {
+                return [
+                    // Order is optional. If not specified it will be assigned automatically
+
+                    {
+                        name: 'Vachirakorn',
+                        tel: '0882391875',
+                        email: 'vachirakorn@hotmail.com',
+                        children: ['overlap1'],
+                        tasks: [{
+                            name: 'learn angular-gantt',
+                            project: 'Internship',
+                            color: '#ff99cc',
+                            from: new Date(2016, 4, 30, 8, 0, 0),
+                            to: new Date(2016, 5, 2, 18, 30, 0),
+                            priority: 'high',
+                            type: 'BR',
+                            supervisor: 'Chadchai,Mansari',
+                            marketing: 'Namtip',
+                            manday: '6',
+                            progress: 100
+                        }, {
+                            name: 'analysis requirements',
+                            project: 'Internship',
+                            color: '#ff99cc',
+                            from: new Date(2016, 5, 3, 8, 0, 0),
+                            to: new Date(2016, 5, 5, 18, 30, 0),
+                            priority: 'high',
+                            type: 'BR',
+                            supervisor: 'Chadchai,Mansari',
+                            marketing: 'Namtip',
+                            manday: '6',
+                            progress: 100
+                        }, {
+                            name: 'discuss design prototype',
+                            project: 'Internship',
+                            color: '#ff99cc',
+                            from: new Date(2016, 5, 6, 8, 0, 0),
+                            to: new Date(2016, 5, 7, 18, 30, 0),
+                            priority: 'high',
+                            type: 'BR',
+                            supervisor: 'Chadchai,Mansari',
+                            marketing: 'Namtip',
+                            manday: '6',
+                            progress: 100
+                        }, {
+                            name: 'Design new UI',
+                            project: 'Internship',
+                            color: '#1abc9c',
+                            from: new Date(2016, 5, 8, 8, 0, 0),
+                            to: new Date(2016, 5, 14, 18, 30, 0),
+                            priority: 'high',
+                            type: 'IR',
+                            supervisor: 'Chadchai,Mansari',
+                            marketing: 'Namtip',
+                            manday: '5',
+                            progress: 100
+                        }, {
+                            name: 'Coding DB connection',
+                            project: 'Internship',
+                            color: '#ffcc00',
+                            from: new Date(2016, 5, 15, 8, 0, 0),
+                            to: new Date(2016, 5, 23, 18, 30, 0),
+                            priority: 'high',
+                            type: 'IR',
+                            supervisor: 'Chadchai,Mansari',
+                            marketing: 'Namtip',
+                            manday: '7'
+                        }],
+                        utilization: 70
+
+                    },  {
+                        name: 'Sompong',
+                        tasks: [{
+                            name: 'QA',
+                            project: 'Internship',
+                            color: '#cc33ff',
+                            from: new Date(2016, 5, 19, 8, 0, 0),
+                            to: new Date(2016, 5, 27, 18, 30, 0),
+                            priority: 'high',
+                            type: 'IR',
+                            supervisor: 'Chadchai,Mansari',
+                            marketing: 'Namtip',
+                            manday: '21.8'
+
+                        }],
+                        utilization: 30
+                    }, {
+                        name: 'Somchai',
+                        tasks: [{
+                            name: 'Support',
+                            project: 'E-GOLD',
+                            color: '#dd44cc',
+                            from: new Date(2016, 4, 30, 8, 0, 0),
+                            to: new Date(2016, 5, 7, 18, 30, 0),
+                            priority: 'high',
+                            type: 'SR',
+                            supervisor: 'Chadchai,Mansari',
+                            marketing: 'Namtip',
+                            manday: '15.7'
+                        }],
+                        utilization: 30
+                    }, {
+                        name: 'Somsri',
+                        tasks: [{
+                            name: 'Android UI',
+                            project: 'settrade',
+                            color: '#f32d3a',
+                            from: new Date(2016, 4, 28, 8, 0, 0),
+                            to: new Date(2016, 5, 20, 18, 30, 0),
+                            priority: 'high',
+                            type: 'IR',
+                            supervisor: 'Chadchai,Mansari',
+                            marketing: 'Namtip',
+                            manday: '11.6'
+
+                        }],
+                        utilization: 30
+                    }
+                ];
+            },
+            getSampleTimespans: function() {
+                return [{
+                    from: new Date(2016, 4, 30, 8, 0, 0),
+                    to: new Date(2016, 5, 7, 15, 0, 0),
+                    name: 'Learning phase'
+                        //priority: undefined,
+                        //classes: [],
+                        //data: undefined
+                }];
+            },
+            getProjectSampleData: function() {
+                return [{
+                    name: 'Internship',
+                    height: '3em',
+                    // sortable: false,
+                    // drawTask: false,
+                    classes: 'gantt-row-milestone',
+                    color: '#45607D',
+                    children:['Vachirakorn','Sompong'],
+                    data: 'Can contain any custom data or object'
+                },{
+                    name: 'Vachirakorn',
+                    tel: '0882391875',
+                    email: 'vachirakorn@hotmail.com',
+                    children: ['overlap1'],
+                    tasks: [{
+                        name: 'learn angular-gantt',
+                        project: 'Internship',
+                        color: '#ff99cc',
+                        from: new Date(2016, 4, 30, 8, 0, 0),
+                        to: new Date(2016, 5, 2, 18, 30, 0),
+                        priority: 'high',
+                        type: 'BR',
+                        supervisor: 'Chadchai,Mansari',
+                        marketing: 'Namtip',
+                        manday: '6',
+                        progress: 100
+                    }, {
+                        name: 'analysis requirements',
+                        project: 'Internship',
+                        color: '#ff99cc',
+                        from: new Date(2016, 5, 3, 8, 0, 0),
+                        to: new Date(2016, 5, 5, 18, 30, 0),
+                        priority: 'high',
+                        type: 'BR',
+                        supervisor: 'Chadchai,Mansari',
+                        marketing: 'Namtip',
+                        manday: '6',
+                        progress: 100
+                    }, {
+                        name: 'discuss design prototype',
+                        project: 'Internship',
+                        color: '#ff99cc',
+                        from: new Date(2016, 5, 6, 8, 0, 0),
+                        to: new Date(2016, 5, 7, 18, 30, 0),
+                        priority: 'high',
+                        type: 'BR',
+                        supervisor: 'Chadchai,Mansari',
+                        marketing: 'Namtip',
+                        manday: '6',
+                        progress: 100
+                    }, {
+                        name: 'Design new UI',
+                        project: 'Internship',
+                        color: '#1abc9c',
+                        from: new Date(2016, 5, 8, 8, 0, 0),
+                        to: new Date(2016, 5, 14, 18, 30, 0),
+                        priority: 'high',
+                        type: 'IR',
+                        supervisor: 'Chadchai,Mansari',
+                        marketing: 'Namtip',
+                        manday: '5',
+                        progress: 100
+                    }, {
+                        name: 'Coding DB connection',
+                        project: 'Internship',
+                        color: '#ffcc00',
+                        from: new Date(2016, 5, 15, 8, 0, 0),
+                        to: new Date(2016, 5, 23, 18, 30, 0),
+                        priority: 'high',
+                        type: 'IR',
+                        supervisor: 'Chadchai,Mansari',
+                        marketing: 'Namtip',
+                        manday: '7'
+                    }],
+                    utilization: 70
+
+                },  {
+                    name: 'Sompong',
+                    tasks: [{
+                        name: 'QA',
+                        project: 'Internship',
+                        color: '#cc33ff',
+                        from: new Date(2016, 5, 19, 8, 0, 0),
+                        to: new Date(2016, 5, 27, 18, 30, 0),
+                        priority: 'high',
+                        type: 'IR',
+                        supervisor: 'Chadchai,Mansari',
+                        marketing: 'Namtip',
+                        manday: '21.8'
+
+                    }],
+                    utilization: 30
+                }];
+
+            }
+
+        };
+    });
